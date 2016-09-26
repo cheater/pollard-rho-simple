592a37d7ea00dc56f23e5d03f98dcee9097fa318
cheater <195390+cheater@users.noreply.github.com>
2016-09-26 04:09:12 +0200

    better test case output on fail

--- tests/Properties.hs
+++ tests/Properties.hs
@@ -2,10 +2,9 @@ module Properties (properties) where
 
 import Test.Framework (Test)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
-import Test.QuickCheck ((==>), verbose, once, within)
+import Test.QuickCheck ((==>), (===), verbose, once, within)
 import Crypto.Math.DiscreteLog (
     solve_discrete_log
-  , check_log
   , check_is_generator_in_zmodm_mult
   )
 import Data.Maybe (isJust, fromJust)
@@ -21,6 +20,8 @@ isPrime n = loop 2 where
     | n `rem` d == 0 = False
     | otherwise      = loop $ d+1
 
+check_log_qc alpha beta gamma m = ((alpha ^ gamma) `mod` m) === (beta `mod` m)
+
 prop_guess_gamma n alpha gamma =
   verbose.(within (2 * 1000 * 1000)) $
   (n > 1) ==>
@@ -31,7 +32,7 @@ prop_guess_gamma n alpha gamma =
   (check_is_generator_in_zmodm_mult alpha m) ==>
 --  ((alpha `mod` m) > 1) ==>
   (isJust guessed_gamma) ==>
-  (check_log alpha beta (fromJust guessed_gamma) m)
+  (check_log_qc alpha beta (fromJust guessed_gamma) m)
   where
     beta = (alpha ^ gamma) `mod` m
     m = n+1
